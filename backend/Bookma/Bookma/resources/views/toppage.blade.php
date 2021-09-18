e37bfb412c8342396dce7b4710d4eda9a9a98884
KOBAYASHI5963 <kobayan567666@gmail.com>
2021-09-18 20:04:53 +0900

    トップページ

--- backend/Bookma/Bookma/resources/views/toppage.blade.php
+++ backend/Bookma/Bookma/resources/views/toppage.blade.php
@@ -60,25 +60,32 @@
   <div id="column">
     <h5 class=" py-3">新しく出品された本</h5>
   </div>
+
   <div class="row">
     <div class="col-md-2 col-12 ">
       <img  src="https://amd-pctr.c.yimg.jp/r/iwiz-amd/20210902-00000035-san-000-8-view.jpg" style="width:200px; height:50;" alt="">
     </div>
+
     <div class="col-md-2 col-12 ">
       <img src="https://images-na.ssl-images-amazon.com/images/I/81wKgx+I5fL.jpg" style="width:200px; height:50;" alt="">
     </div>
+
     <div class="col-md-2 col-12 ">
       <img src="https://hr-engagement.jp/wp-content/uploads/2020/05/29043be388f971cef4025ecbcc5c9c57.jpg" style="width:200px; height:50;" alt="">
     </div>
+
     <div class="col-md-2 col-12 ">
       <img src="https://images-na.ssl-images-amazon.com/images/I/51sPGsTuF0L._SX350_BO1,204,203,200_.jpg" style="width:200px; height:50;" alt="">
     </div>
+
     <div class="col-md-2 col-12 ">
       <img src="https://images-na.ssl-images-amazon.com/images/I/51VPFSM57AL.jpg" style="width:200px; height:50;" alt="">
     </div>
+
     <div class="col-md-2 col-12 ">
       <img src="https://images-na.ssl-images-amazon.com/images/I/71AaRkqJI8L.jpg" style="width:200px; height:50;" alt="">
     </div>
+    
   </div>
   </div>
 
