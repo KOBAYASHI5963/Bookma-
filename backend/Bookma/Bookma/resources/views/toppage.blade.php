0a1a11136d82ac69dfbb87a038a8ac626ae02508
KOBAYASHI5963 <kobayan567666@gmail.com>
2021-10-10 16:12:43 +0900

    トップページ

--- backend/Bookma/Bookma/resources/views/toppage.blade.php
+++ backend/Bookma/Bookma/resources/views/toppage.blade.php
@@ -112,10 +112,34 @@
 
       <div id="column">
         <h5 class=" py-3">出品一覧</h5>
-              <a class="nav-link" href="#"  aria-expanded="false">
-              Bookma! ʕ•ᴥ•ʔ
-              </a>
-              
+
+
+      <div class="row">
+        <div class="col-md-2 col-12 ">
+          <img  src="https://images-na.ssl-images-amazon.com/images/I/61iQ1gOJ7kL._SX316_BO1,204,203,200_.jpg" style="width:200px; height:50;" alt="">
+        </div>
+
+        <div class="col-md-2 col-12 ">
+          <img src="https://images-na.ssl-images-amazon.com/images/I/81wKgx+I5fL.jpg" style="width:200px; height:50;" alt="">
+        </div>
+
+        <div class="col-md-2 col-12 ">
+          <img src="https://hr-engagement.jp/wp-content/uploads/2020/05/29043be388f971cef4025ecbcc5c9c57.jpg" style="width:200px; height:50;" alt="">
+        </div>
+
+        <div class="col-md-2 col-12 ">
+          <img src="https://images-na.ssl-images-amazon.com/images/I/51sPGsTuF0L._SX350_BO1,204,203,200_.jpg" style="width:200px; height:50;" alt="">
+        </div>
+
+        <div class="col-md-2 col-12 ">
+          <img src="https://images-na.ssl-images-amazon.com/images/I/51VPFSM57AL.jpg" style="width:200px; height:50;" alt="">
+        </div>
+
+        <div class="col-md-2 col-12 ">
+          <img src="https://images-na.ssl-images-amazon.com/images/I/71AaRkqJI8L.jpg" style="width:200px; height:50;" alt="">
+        </div>
+
+             
       </div>
   
 
