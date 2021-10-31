b3c5bb76390500900cb363ddb8f054fe844035e2
KOBAYASHI5963 <kobayan567666@gmail.com>
2021-10-31 22:11:39 +0900

    購入履歴

---
+++ backend/Bookma/Bookma/resources/views/conponents/myPage/purchasedItem.blade.php
@@ -0,0 +1,16 @@
+<div class="mb-3">
+  <h3>購入した商品</h3>
+</div>
+
+
+<div class="row">
+
+    <div class="col-sm">
+    <p>取引中</p>
+    </div>
+    
+    <div class="col-sm">
+    <p>過去の取引</p>
+    </div>
+
+</div>
--- backend/Bookma/Bookma/resources/views/pages/myPage/profileEdit.blade.php
+++ backend/Bookma/Bookma/resources/views/pages/myPage/profileEdit.blade.php
@@ -4,8 +4,17 @@
 <div class="container mt-3">
 
 <!-- ここにページ毎のコンテンツを書く -->
-@include('conponents.myPage.profileEdit')
-  
+
+@include('conponents.myPage.purchaserMenuButton')
+  <div class="row  mt-5">
+    <div class="col-sm-3 col-xs-12">
+     @include('conponents.myPage.purchaserMenu')
+    </div>
+    <div class="col-sm-9 col-xs-12">
+
+     @include('conponents.myPage.profileEdit')
+    </div>
+  </div> 
 
 </div>
 
