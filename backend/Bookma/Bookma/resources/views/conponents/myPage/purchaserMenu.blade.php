443a212fe11ffb8047c604b70df20e999a856334
KOBAYASHI5963 <kobayan567666@gmail.com>
2021-11-18 21:49:08 +0900

    delete_profile

--- backend/Bookma/Bookma/resources/views/conponents/myPage/purchaserMenu.blade.php
+++ backend/Bookma/Bookma/resources/views/conponents/myPage/purchaserMenu.blade.php
@@ -6,7 +6,6 @@
 
 <div class="card my-3">
     <ul class="list-group list-group-flush accout_setting">
-      <a href=""><li class="list-group-item">プロフィール</li></a>
       <a href="{{ route('profileEdit') }}"><li class="list-group-item">プロフィール編集</li></a>
       <a href="{{ route('purchaseHistory_transaction') }}"><li class="list-group-item">購入履歴</li></a>
       <a href="{{ route('favorites') }}"><li class="list-group-item">お気に入り一覧</li></a>
