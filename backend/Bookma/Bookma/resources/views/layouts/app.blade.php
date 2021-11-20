f64e20471e5d46b29e2d9f20e984eba4076b10e9
kawada <vgnsl94@gmail.com>
2021-11-21 01:09:46 +0900

    fix 全体のcss変更

--- backend/Bookma/Bookma/resources/views/layouts/app.blade.php
+++ backend/Bookma/Bookma/resources/views/layouts/app.blade.php
@@ -10,11 +10,23 @@
 
     <body>
       @include('common.header')
-      @yield('content')
+      <main>
+        @yield('content')
+      </main>
       @include('common.footer')
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
         <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
     </body>
+    <style>
+      body {
+        display: flex;
+        flex-flow: column;
+        min-height: 100vh;
+      }
+      main {
+        flex: 1;
+      }
+    </style>
 </html>
