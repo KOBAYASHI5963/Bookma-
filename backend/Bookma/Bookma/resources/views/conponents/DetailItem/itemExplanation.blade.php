9716ddca49b7ac3770152cfc88a54f05c4fc1ad1
KOBAYASHI5963 <kobayan567666@gmail.com>
2021-11-21 16:05:21 +0900

    ‘feature/purchasedItemList’

--- backend/Bookma/Bookma/resources/views/conponents/DetailItem/itemExplanation.blade.php
+++ backend/Bookma/Bookma/resources/views/conponents/DetailItem/itemExplanation.blade.php
@@ -1,20 +1,79 @@
 <div>
   <h3>呪術廻戦1巻</h3>
 </div>
+
 <div class="mt-3">
   <h5>¥350 (税込) 送料込み</h>
 </div>
 
-<div class="mt-3">
+<button type="button" class="btn btn-secondary btn-lg" disabled>売り切れました</button>
+
+<div class="mt-5">
       <h4>商品の説明</h>
 </div>
 
-<div>
-  <p>「呪術廻戦 1 両面宿儺」</p>
-  <p>芥見下々</p>
-  <p>定価: ￥ 484</p>
-</div>
+  <ul class='list-unstyled'>  
+    <li>「呪術廻戦 1 両面宿儺」</li>
+    <li>芥見下々</li>
+    <li>定価: ￥ 484</li>
+  </ul>  
 
 <div>
   <p>類稀な身体能力を持つ高校生・虎杖悠仁は、病床に伏せる祖父の見舞いを日課にしていた。だがある日学校に眠る「呪物」の封印が解かれ、化物が現れてしまう。取り残された先輩を救う為、校舎へ乗り込む虎杖だが!?</p>
 </div>
+
+<div class="mt-5">
+  <h4>商品の情報</h>
+</div>
+
+<div class="row">
+  <div class="col-4">
+    <div class="mt-4">
+      <p>カテゴリー</p>
+    </div>
+    <div class="mt-5">
+      <p>商品の状態</p>
+      <p>配送料の負担</p>
+      <p>配送の方法</p>
+      <p>発送元の地域</p>
+      <p>発送までの日数</p>
+    </div>
+  </div>
+
+  <div class="col-8">
+    <ul class='list-unstyled'>  
+      <a href=""><li>本・音楽・ゲーム</li></a>
+      <a href=""><li>漫画</li></a>
+      <a href=""><li>少年漫画</li></a>
+      <li class="mt-4">目立った傷や汚れなし</li>
+      <li class="mt-3">送料込み(出品者負担)</li>
+      <li class="mt-3">らくらくブックマ便</li>
+      <li class="mt-3">広島県</li>
+      <li class="mt-3">2~3日で発送</li>
+    </ul>  
+  </div>
+<div>
+
+<div class="mt-4">
+  <h4>出品者</h>
+</div>
+
+<div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">田辺さん</h5>
+        <p class="card-text">★★★★★534</p>
+      </div>
+    </div>
+  </div>
+</div>
+  
+<div class="mt-4">
+  <h4>コメント</h>
+</div>
+<p>コメントはありません</p>
+<p class="mt-3">※売り切れのためコメントできません</p>
