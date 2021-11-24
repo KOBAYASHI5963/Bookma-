3c073c54a6c8d91e644e67f2b0d8d34d391aa62f
KOBAYASHI5963 <kobayan567666@gmail.com>
2021-11-24 23:26:54 +0900

    ‘フォローリスト’

--- backend/Bookma/Bookma/resources/views/conponents/myPage/follow.blade.php
+++ backend/Bookma/Bookma/resources/views/conponents/myPage/follow.blade.php
@@ -1,12 +1,37 @@
 <div class="mb-3">
-  <h3>フォロー一覧</h3>
+  <h3>フォローリスト</h3>
 </div>
 
-  <ul class="nav">
-    <div class="col-6">
-      <li><a href="#">フォロー</a></li>
+
+<div class="card mb-3">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." alt="...">
     </div>
-    <div class="col-6">
-      <li><a href="#">フォロワー</a></li>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">田辺さん</h5>
+        <p class="card-text">★★★★★534</p>
+        <a class="btn btn-primary btn" href="#" >フォロー中</a>
+        <a class="btn btn-danger btn" href="#" >フォローを解除</a>
+      </div>
     </div>
-  </ul>
+  </div>
+</div>
+
+
+<div class="card mb-3">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="..." alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">YOSHIKO</h5>
+        <p class="card-text">★★★★★88</p>
+        <a class="btn btn-primary btn" href="#" >フォロー中</a>
+        <a class="btn btn-danger btn" href="#" >フォローを解除</a>
+      </div>
+    </div>
+  </div>
+</div>
