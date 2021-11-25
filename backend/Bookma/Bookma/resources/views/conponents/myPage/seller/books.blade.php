2457194c80f96fc5d28605767bf15ec824f8ccf5
KOBAYASHI5963 <kobayan567666@gmail.com>
2021-11-25 19:16:38 +0900

    ‘出品本’

--- backend/Bookma/Bookma/resources/views/conponents/myPage/seller/books.blade.php
+++ backend/Bookma/Bookma/resources/views/conponents/myPage/seller/books.blade.php
@@ -1,1 +1,39 @@
-ログインユーザーの出品本一覧
+<div class="mb-3">
+  <h3>ログインユーザーの出品本一覧</h3>
+</div>
+
+
+
+<div class="card mb-3 mt-3">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">1%の努力</h5>
+        <p class="card-text">¥1500</p>
+        <div>
+          <a class="btn btn-success btn" href="#" >編集</a>
+          <a class="btn btn-danger btn" href="#" >削除</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<nav aria-label="...">
+  <ul class="pagination">
+    <li class="page-item disabled">
+      <span class="page-link"><</span>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item active" aria-current="page">
+      <span class="page-link">2</span>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#">></a>
+    </li>
+  </ul>
+</nav>
