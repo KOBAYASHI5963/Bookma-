5be93e9d6f03ebafe23a0a90a4a8ebc9e73b730d
KOBAYASHI5963 <kobayan567666@gmail.com>
2022-02-26 22:23:11 +0900

    ‘マイページのプロフィール画像表示’

--- backend/Bookma/Bookma/resources/views/pages/myPage/seller/books.blade.php
+++ backend/Bookma/Bookma/resources/views/pages/myPage/seller/books.blade.php
@@ -8,7 +8,10 @@
 @include('conponents.myPage.seller.purchaserMenuButton')
   <div class="row  mt-5">
     <div class="col-sm-3 col-xs-12">
-     @include('conponents.myPage.seller.purchaserMenu')
+     @include('conponents.myPage.seller.purchaserMenu',
+     [
+      ''
+     ])
     </div>
     <div class="col-sm-9 col-xs-12">
      @include('conponents.myPage.seller.books',
