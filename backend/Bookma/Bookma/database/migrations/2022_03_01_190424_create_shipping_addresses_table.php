9d7c66822557dc5f763b23e69726109d58054b4f
kawada <vgnsl94@gmail.com>
2022-03-13 00:35:43 +0900

    migrationファイル修正

--- backend/Bookma/Bookma/database/migrations/2022_03_01_190424_create_shipping_addresses_table.php
+++ backend/Bookma/Bookma/database/migrations/2022_03_01_190424_create_shipping_addresses_table.php
@@ -19,12 +19,15 @@ class CreateShippingAddressesTable extends Migration
             $table->foreignId('user_id')->constrained()->comment('外部キー');
             $table->string('name', 50)->comment('氏名');
             $table->integer('post_code')->comment('郵便番号');
-            $table->foreignId('prefecture')->constrained()->comment('都道府県 ex.東京都');
+            $table->bigInteger('prefecture')->unsigned()->comment('都道府県 ex.東京都');
             $table->string('city')->comment('市区町村 ex.港区');
             $table->string('street')->comment('番地 ex.青山1-2-3');
             $table->string('building_name')->nullable($value = true)->comment('建物名(任意) ex.レブロンハイツ325');
             $table->unsignedBigInteger('phone_number')->comment('電話番号');
             $table->timestamps();
+
+            // 外部キー
+            $table->foreign('prefecture')->references('id')->on('shipping_areas')->onUpdate('CASCADE')->onDelete('CASCADE');
         });
     }
 
--- backend/Bookma/Bookma/resources/views/conponents/myPage/shippingAddressList.blade.php
+++ backend/Bookma/Bookma/resources/views/conponents/myPage/shippingAddressList.blade.php
@@ -14,12 +14,12 @@
   <div class="row g-0">
     <div class="col-md-8">
       <div class="card-body">
-      <div style=”line-height:1em;”>{{ $shippingAddressList->name }}</div>
-      <div style=”line-height:1em;”>{{ $shippingAddressList->post_code }}</div>
-      <div style=”line-height:1em;”>{{ $shippingAddressList->shippingArea->area}}{{ $shippingAddressList->city }}</div>
-      <div style=”line-height:1em;”>{{ $shippingAddressList->street }}</div>
-      <div style=”line-height:1em;”>{{ $shippingAddressList->building_name }}</div>
-      <div style=”line-height:1em;”>{{ $shippingAddressList->phone_number }}</div>
+      <div style=”line-height:1em;”><span style="font-weight: bold;">氏名：</span>{{ $shippingAddressList->name }}</div>
+      <div style=”line-height:1em;”><span style="font-weight: bold;">郵便番号：</span>{{ $shippingAddressList->post_code }}</div>
+      <div style=”line-height:1em;”><span style="font-weight: bold;">都道府県市区町村：</span>{{ $shippingAddressList->shippingArea->area}}{{ $shippingAddressList->city }}</div>
+      <div style=”line-height:1em;”><span style="font-weight: bold;">番地：</span>{{ $shippingAddressList->street }}</div>
+      <div style=”line-height:1em;”><span style="font-weight: bold;">建物目：</span>{{ $shippingAddressList->building_name }}</div>
+      <div style=”line-height:1em;”><span style="font-weight: bold;">電話番号：</span>{{ $shippingAddressList->phone_number }}</div>
 
       <div class="d-flex justify-content-start mt-1">
         <a class="btn btn-success btn mr-2" href="{{ route('shippingAddressEdit', ['id' => $shippingAddressList->id]) }}" >編集</a>
