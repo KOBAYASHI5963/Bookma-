bf557819d2c744f9f94af2cef74bb7c15f4d499f
kawada <vgnsl94@gmail.com>
2022-05-03 16:07:18 +0900

    いらない２

--- backend/Bookma/Bookma/app/Http/Controllers/TopController.php
+++ backend/Bookma/Bookma/app/Http/Controllers/TopController.php
@@ -46,6 +46,7 @@ class TopController extends Controller
 
 
         //いらないコメント
+        //いらないコメント2
         // $request->input()で検索時に入力した項目を取得
         $searchKeyword = $request->input('keyword');
         $searchCategory = $request->input('category_id');
