f5d6a413efb6bfe441081a958e91eb6717dad051
kawada <vgnsl94@gmail.com>
2022-05-03 16:55:24 +0900

    必要2

---
+++ backend/Bookma/Bookma/database/migrations/2022_04_27_150150_add_test_to_books_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Book;
+
+class AddTestToBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->string('test')->after('content');
+            $table->string('test2')->after('test');
+        });
+
+        $books = Book::all();
+
+        foreach ($books as $bookImage){
+            $bookImage->test = 'test';
+            $bookImage->save();
+         }
+         
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->dropColumn('test');
+        });
+    }
+}
