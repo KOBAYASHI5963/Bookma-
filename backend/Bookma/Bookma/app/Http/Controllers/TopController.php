5e67f862fd17de724d2a5d58c2c824e0400dd6ca
kawada <vgnsl94@gmail.com>
2022-05-03 17:03:09 +0900

    いらない

--- backend/Bookma/Bookma/app/Http/Controllers/TopController.php
+++ backend/Bookma/Bookma/app/Http/Controllers/TopController.php
@@ -180,6 +180,8 @@ class TopController extends Controller
             }
         }
 
+        // いらない
+
         // A and C ⇨もし「キーワード」かつ「プルダウン」が入力されていたら(キーワード、プライス)
         if($request->keyword && (!$request->category_id && !$request->product_condition) && $request->price)
         {
