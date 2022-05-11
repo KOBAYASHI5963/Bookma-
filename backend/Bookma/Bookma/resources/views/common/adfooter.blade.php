1bbc84e80bc8a41f60915b2cd55f0809f255f245
KOBAYASHI5963 <kobayan567666@gmail.com>
2022-05-11 23:47:41 +0900

    ‘管理者画面ヘッダー、フッター’

---
+++ backend/Bookma/Bookma/resources/views/common/adfooter.blade.php
@@ -0,0 +1,22 @@
+<footer id="footer" class="footer outer-block">
+  <div class="inner-block">
+  </div><!-- /inner-block -->
+      <div class="copyright mt-2">
+        COPYRIGHT © Bookma Inc. All rights Reserved.
+      </div>
+</footer>
+
+<style scoped>
+  
+#footer {
+  background: #222;
+}
+
+  #footer .copyright {
+    color: #ffffff;
+    height: 40px;
+    text-align: center
+    
+  }
+
+  </style>
--- backend/Bookma/Bookma/resources/views/pages/ administrator/ administrator.blade.php
+++ backend/Bookma/Bookma/resources/views/pages/ administrator/ administrator.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.ad')
 
 @section('content')
 <div class="container mt-3">
